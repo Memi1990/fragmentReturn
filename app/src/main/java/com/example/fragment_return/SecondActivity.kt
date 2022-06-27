cb83cd5ae7c799af5133730599574f568ceeccf9
Alumno <noemilopezpina@gmail.com>
2022-06-27 20:32:23 +0200

    commit animaciones

--- app/src/main/java/com/example/fragment_return/SecondActivity.kt
+++ app/src/main/java/com/example/fragment_return/SecondActivity.kt
@@ -20,19 +20,32 @@ class SecondActivity : AppCompatActivity() {
         val frag4 = FourthFragment()
         val frag5 = FifthFragment()
 
-        b.btnBlue.setOnClickListener {fragmentTrans(frag3)}
-        b.btnBeige.setOnClickListener {fragmentTrans(frag4)}
-        b.btnBrown.setOnClickListener {fragmentTrans(frag5)}
+        b.btnBlue.setOnClickListener {fragmentTransCustom(frag3)}
+        b.btnBeige.setOnClickListener {fragmentTransCustom(frag4)}
+        b.btnBrown.setOnClickListener {fragmentTransCustom(frag5)}
 
         b.btnBack.setOnClickListener {
             onBackPressed()
         }
     }
-    fun fragmentTrans(fragment:Fragment){
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragmentContainerView3,fragment)
-            addToBackStack(null)
-            commit()
-        }
+//    fun fragmentTrans(fragment:Fragment){
+//        supportFragmentManager.beginTransaction().apply {
+//            replace(R.id.fragmentContainerView3,fragment)
+////            Poner en la pila. Eso repercute a la hora de volver
+//            addToBackStack(null)
+//            commit()
+//        }
+//    }
+    fun fragmentTransCustom(fragment:Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+            .setCustomAnimations(
+                R.anim.slide_in,
+                R.anim.fade_in,
+                R.anim.slide_out,
+                R.anim.fade_out)
+            .replace(R.id.fragmentContainerView3,fragment)
+            .commit()
     }
 }
