7cbaddae41cbc76ffe2afe658523384e9f422ae8
Alumno <noemilopezpina@gmail.com>
2022-06-28 19:14:19 +0200

    commit menu

--- app/src/main/java/com/example/fragment_return/Fragments/FifthFragment.kt
+++ app/src/main/java/com/example/fragment_return/Fragments/FifthFragment.kt
@@ -1,33 +1,32 @@
 package com.example.fragment_return.Fragments
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.fragment_return.R
+import com.google.android.material.snackbar.Snackbar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FifthFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FifthFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private var myView:View? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_fragment1,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        super.onOptionsItemSelected(item)
+        when(item.itemId){
+            R.id.itCopy-> Toast.makeText(context, "Has copiado esto", Toast.LENGTH_SHORT).show()
+            R.id.itCut-> Snackbar.make(myView!!, "Has cortado esto", Snackbar.LENGTH_SHORT).show()
+            R.id.itPaste-> Snackbar.make(myView!!, "Has pegado esto", Snackbar.LENGTH_SHORT).show()
         }
+        return super.onContextItemSelected(item)
     }
 
     override fun onCreateView(
@@ -35,26 +34,8 @@ class FifthFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fifth, container, false)
+        myView = inflater.inflate(R.layout.fragment_first, container, false)
+        return myView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FifthFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FifthFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
